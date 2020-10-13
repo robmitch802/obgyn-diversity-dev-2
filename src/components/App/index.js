d47d3732af590f33a9c3833a140ac2970603718e
robmitch802 <60521912+robmitch802@users.noreply.github.com>
2020-10-13 16:20:00 -0400

    basic setup

---
+++ src/components/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MyEditor from '../MyEditor';
+
+const App = () => (
+    <div className="container" >
+        <h1>App</h1>
+        <MyEditor />
+    </div>
+)
+
+export default App;
